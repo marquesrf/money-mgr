461c3b249fd5b851981971f583eff4c24e7699f9
Rafael Marques <marquesrf@protonmail.com>
2020-03-30 23:33:53 -0300

    Added test to insert user

--- test/user.test.js
+++ test/user.test.js
@@ -10,3 +10,13 @@ test("Must list all users", () => {
       expect(res.body[0]).toHaveProperty("name", "John Doe");
     });
 });
+
+test("Must insert a user", () => {
+  return request(app)
+    .post("/users")
+    .send({ name: "Walter Mitty", mail: "walter@mail.com" })
+    .then(res => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe("Walter Mitty");
+    });
+});
